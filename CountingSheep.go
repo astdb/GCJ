983fcd9b6bc96c5513a366cbe98bda7b7efbe749
Nirmal <astdb@users.noreply.github.com>
2017-04-04 21:33:15 +1000

    Commit #2

--- CountingSheep.go
+++ CountingSheep.go
@@ -1,4 +1,3 @@
-
 // https://code.google.com/codejam/contest/6254486/dashboard
 
 package main
@@ -7,16 +6,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
+	"sort"
 )
 
-func main(){
+func main() {
 	// ensure input filename entered on command line
 	if len(os.Args) <= 1 {
 		fmt.Println("Usage: > go run CountingSheep.go <input.file>")
 		return
 	}
+
+	//----------------------------------test---------------------------------
+	// fmt.Printf("%v\n", getDigits(232168))
+	// return
+	//---------------------------------endtest-------------------------------
 
 	// capture input file name
 	file, err := os.Open(os.Args[1])
@@ -29,62 +34,122 @@ func main(){
 	// setup scanner to read input file
 	scanner := bufio.NewScanner(file)
 
-	k := 0 // line numbers 
+	k := 0 // line numbers
 	testcasecount := 0
 	for scanner.Scan() {
 		if k == 0 {
 			// read number of testcases
 			testcasecount, err = strconv.Atoi(strings.TrimSpace(scanner.Text()))
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error reading input file: %v\n", err)
+				fmt.Fprintf(os.Stderr, "Error reading testcase count: %v\n", err)
 				return
 			}
+			k++
+			continue
 		} else {
 			if k > testcasecount {
 				break
 			}
 		}
-		
-		// read next line
+
+		// read next testcase
 		n, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading input file: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Error reading testcase: %v\n", err)
 			return
 		}
 
+		// process case: start counting
 		i := 1
-		for {
-			
+		// n_orig := n
+		digits := []int{}
+		for !complete(digits) {
+			val := n * i // OVERFLOW??
+			val_digits := getDigits(val)
+
+			if (i > 1) && (n == val) {
+				fmt.Printf("Case #%d: INSOMNIA\n", k)
+				break
+			}
+
+			for _, v := range val_digits {
+				if !contains(digits, v) {
+					digits = append(digits, v)
+
+					if complete(digits) {
+						fmt.Printf("Case #%d: %d\n", k, val)
+					}
+				}
+			}
+
 			i++
 		}
 
+		// increment testcase count
 		k++
 	}
 }
 
-// takes a positive integer and returns an int slice containing all unique decimal digits of it 
+// check if an int slice contains a given number
+func contains(digits []int, n int) bool {
+	for _, v := range digits {
+		if v == n {
+			return true
+		}
+	}
+
+	return false
+}
+
+// check if a given int slice has all the digits 0-9
+func complete(digits []int) bool {
+	if len(digits) != 10 {
+		return false
+	}
+
+	sort.Ints(digits)
+	j := 0
+	for _, v := range digits {
+		if v != j {
+			return false
+		}
+
+		j++
+	}
+
+	return true
+}
+
+// takes a positive integer and returns an int slice containing all unique decimal digits of it
 func getDigits(n int) []int {
 	if n < 0 {
 		return nil
 	}
 
+	rem := n % 10
 	digits := []int{}
-	rem := n%10
 	digits = append(digits, rem)
 
 	for rem > 0 {
-		rem = rem%2
+		// rem = rem % 2
+		n = n / 10
+		rem = n % 10
 
 		in := false
-		for _, v := range digits {
-			if v == rem {
-				in = true
-				break
-			}
+		// for _, v := range digits {
+		// 	if v == rem {
+		// 		in = true
+		// 		break
+		// 	}
+		// }
+		if contains(digits, rem) {
+			break
 		}
 
 		if !in {
 			digits = append(digits, rem)
 		}
 	}
+
+	return digits
 }
